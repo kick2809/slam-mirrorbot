d718742fd44aaba2b490515198ff08e19f32f42d
kick2809 <83411241+kick2809@users.noreply.github.com>
2021-07-11 22:52:22 +0530

    Update count.py

--- bot/modules/count.py
+++ bot/modules/count.py
@@ -22,10 +22,10 @@ def countNode(update, context):
         else:
             uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
         if uname is not None:
-            cc = f'\n\ncc: {uname}'
+            cc = f'\n\n♂/♀/⚣/⚢👉 {uname}'
         sendMessage(result + cc, context.bot, update)
     else:
-        sendMessage("Provide G-Drive Shareable Link to Count.", context.bot, update)
+        sendMessage("BRUH Provide G-Drive Shareable/Open Link to Count.", context.bot, update)
 
 count_handler = CommandHandler(BotCommands.CountCommand, countNode, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 dispatcher.add_handler(count_handler)
